58345591689505673cfef295013a45079d56f768
Alex Cheung <alexc.sfu@gmail.com>
2022-04-20 05:49:25 +0000

    added more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -9,3 +9,14 @@ if (temperature < 0){
 }
 
 console.log("Now you are ready to go outside");
+
+//logical operators below
+if (temperature < -40 || temperature > 40){
+  console.log("Maybe going outside isn't such a good idea...");
+}
+
+let raining = true;
+
+if (!raining){
+  console.log("Leave your umbrella at home!");
+}
